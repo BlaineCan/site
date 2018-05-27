63ca436a32aa4ba23f54283852636a6822587910
Blaine <blaine.cantrell@gmail.com>
2018-05-27 05:30:25 -0500

    started form. Going to bed now

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let drop = document.querySelector('.drop');
-let left = document.querySelector('.left');
 
-function line(){
+(function line(){
+  let drop = document.querySelector('.drop');
   drop.addEventListener('click', function(){
-    left.setAttribute('class', 'hidden');
+    let left = document.querySelector('.left');
+    let friendform = document.querySelector('.friendform');
+    left.className += ' hidden';
+    friendform.className += 'friendform';
   });
-}
-
-line();
+})();
