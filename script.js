afd84cae194d11403b577acb322495f539518056
Blaine <blaine.cantrell@gmail.com>
2018-07-22 12:27:30 -0500

    Being indecisive

---
+++ script.js
@@ -0,0 +1,25 @@
+(function site(){
+    this.init = function (){
+        this.showfriend();
+        this.goback();
+    }
+    this.showfriend = function(){
+        let drop = document.querySelector('.drop');
+        drop.addEventListener('click', function(){
+            let left = document.querySelector('.left');
+            let friendform = document.querySelector('.friendform');
+            left.className += ' hidden';
+            friendform.className += 'friendform';
+        });
+    }
+    this.goback = function(){
+        let backbutton = document.querySelector('.backbutton');
+        backbutton.addEventListener('click', function(){
+            let left = document.querySelector('.left');
+            let friendform = document.querySelector('.friendform');
+            left.className += 'left';
+            friendform.className += ' hidden';
+        });
+    }
+    this.init();
+})(); 
