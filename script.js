e6911cfc74635c2ba05dc5668bc5fd14a1f3d539
blaineCantrell <blaine.cantrell@gmail.com>
2018-08-28 02:03:52 -0500

    Made some changes. Nothing huge.

--- script.js
+++ script.js
@@ -3,15 +3,6 @@
         this.showfriend();
         this.goback();
     }
-    // this.showfriend = function(){
-    //     let drop = document.querySelector('.drop');
-    //     drop.addEventListener('click', function(){
-    //         let left = document.querySelector('.left');
-    //         let friendform = document.querySelector('.friendform');
-    //         left.className += ' hidden';
-    //         friendform.className += 'friendform';
-    //     });
-    // }
     this.showfriend = function(){
       let drop = document.querySelector('.drop');
       drop.addEventListener('click', function(){
