97d2c9ed705d38ad9623927fa1968854c2aead63
Whatchamazog <blaine.cantrell@gmail.com>
2018-10-29 16:02:17 -0500

    Refactored work

--- javascript/work.js
+++ javascript/work.js
@@ -3,23 +3,17 @@ const projects = [
     {title:'Weather', description: 'This is just a silly little weather app that I built with vanilla javascript making a series of api calls.', url:'https://whatchamazog.github.io/weather/'}
 ];
 
-(function showProjects(projects){
-    this.init = ()=>{
-        this.generateList();
-    }
-    this.generateList = ()=>{
-        let container = document.querySelector('article');
-        let template = '';
-        for(let i = 0; i < projects.length; i++){
-            template += `
-            <div>
-                <p><a target='_blank' href='${projects[i].url}'>${projects[i].title}</a></p>
-                <p>${projects[i].description}</p>
-            </div>
-            `;
-        }
-        container.innerHTML = '';
-        container.insertAdjacentHTML('afterbegin', template)
-    } 
-    this.init();
+((projects)=>{
+    let container = document.querySelector('article');
+    let template = '';
+    projects.forEach((projects)=>{
+        template += `
+        <div>
+            <p><a target='_blank' href='${projects.url}'>${projects.title}</a></p>
+            <p>${projects.description}</p>
+        </div>
+        `;
+    })
+    container.innerHTML = '';
+    container.insertAdjacentHTML('afterbegin', template)
 })(projects);
