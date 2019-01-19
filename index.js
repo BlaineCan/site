3609e2f960ca089be5179d01a488bb600a5b63a0
Whatchamazog <blaine.cantrell@gmail.com>
2019-01-19 04:17:42 -0600

    Serving it up

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+let app = express();
+
+app.use(express.static('www'));
+
+let port = process.env.PORT || 8000;
+let listening = () => {
+    console.log("Serving at localhost:" + port);
+}
+
+app.listen(port, listening)
