fe5eef54820d9747797a795e4a0bb3d1686b4802
Whatchamazog <blaine.cantrell@gmail.com>
2019-06-14 23:35:47 -0500

    Removed the www folder. Reorganized some files. Updated paths.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 let app = express();
 
-app.use(express.static('www'));
+app.use(express.static('frontend'));
 
 let port = process.env.PORT || 8800;
 let listening = () => {
